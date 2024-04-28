0261e6f7e1a7b71168bdbe13677c3ce7b5404dfa
VitaLocke <acl112266@gmail.com>
2024-04-28 18:30:06 +0900

    Revert "Merge pull request #3 from hufman/rcImgs_underflow"
    This reverts commit 91ff04014a80fc23c4db0a7e741963fe759f4758, reversing
    changes made to f2967da690122da28f08965811ba3f6d7c103b7b.

--- animEx.cpp
+++ animEx.cpp
@@ -294,17 +294,15 @@ bool splitFiles(const char* cFilename)
 		vector<rectImgHelper> rih;
 		int k = 0;
 		//Loop per rectImg
-		if (rcImgs.size() > 0) {
-			for(int j = 0; j < rcImgs.size()-1; j++)
+		for(int j = 0; j < rcImgs.size()-1; j++)
+		{
+			//Pull all rects in from this image
+			for(; k < rcImgs[j+1].rectStart; k++)
 			{
-				//Pull all rects in from this image
-				for(; k < rcImgs[j+1].rectStart; k++)
-				{
-					rectImgHelper help;
-					help.img = rcImgs[j].img;
-					help.rc = fiRect[k];
-					rih.push_back(help);
-				}
+				rectImgHelper help;
+				help.img = rcImgs[j].img;
+				help.rc = fiRect[k];
+				rih.push_back(help);
 			}
 		}
 		
